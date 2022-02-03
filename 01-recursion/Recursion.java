48b83afed820e2e7d39fc3eab8601fa60295215d
HarryZ <hzhu30@stuy.edu>
2022-02-03 11:32:07 -0500

    I created a loop for the char array so that it will go through all the letters in it.

---
+++ 01-recursion/Recursion.java
@@ -0,0 +1,62 @@
+public class Recursion {
+  /*Print all words that are made of the letters a-e inclusive.
+         *@param length : the length of the words that are to be printed
+         */
+         public static void printAllWords(int length){
+           //printAllWords(length,"");
+           char[] letters = {'a', 'b', 'c', 'd'};
+           printNoDoubleLetterWords(3, letters);
+         }
+
+         /*Print all words that are made of the letters a-e inclusive.
+         *@param length : either how many more letters or the total length depending on implementation
+         *@param word   : the partial word so far.
+         */
+         public static void printAllWords(int length,String word){
+             if (length > 0) {
+               for (char i = 'a'; i <= 'e'; i ++) {
+                 printAllWords(length - 1, word + i);
+               }
+             } else {
+               System.out.println(word);
+             }
+         }
+        public static void main(String[] args) {
+          // printAllWords(3, "");
+          char[] letters = {'a', 'b', 'c', 'd'};
+           printNoDoubleLetterWords(3, letters);
+        }
+
+
+
+
+        /*Print all words that are made of the characters in the array of letters.
+                *There may not be consecutive equal letters, so:
+                *aax is not allowed, but axa is allowed.
+                *@param length : the length of the words that are to be printed
+                *@param letters: the letters you should be using
+                */
+                public static void printNoDoubleLetterWords(int length,char[] letters){
+                  printNoDoubleLetterWords(length,"",letters);
+                }
+
+                /*Print all words that are made of the characters in letters. There may not be consecutive equal letters,
+                *aax is not allowed, but axa is allowed.
+                *@param length : either how many more letters need to be
+                *@param word   : the partial word so far.
+                *@param letters: the letters you should be using
+                */
+                public static void printNoDoubleLetterWords(int length,String word,char[]letters){
+                  //WRITE THIS METHOD
+                  if (length > 0) {
+                    for (int i = 0; i < letters.length; i ++) {
+                      if (i == 0 || word.charAt(i - 1) != word.charAt(i)) {
+                      printNoDoubleLetterWords(length - 1, word + letters[i], letters);
+                    }
+                    }
+                  } else {
+                    System.out.println(word);
+                  }
+                }
+
+        }
