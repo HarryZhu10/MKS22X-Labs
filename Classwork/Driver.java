1ed4343111a7fa9faa29052afa7abb73c704bcb2
HarryZ <hzhu30@stuy.edu>
2022-02-09 10:12:11 -0500

    I added test cases for groupSum, including empty arrays and regular things.

--- Classwork/Driver.java
+++ Classwork/Driver.java
@@ -1,10 +1,13 @@
 public class Driver {
 public static void main(String[] args) {
   int[] nums = {1,2,3,3,6};
+  int[] empty = {};
 System.out.println(RecursionClasswork.groupSum(0, nums, 6));
 System.out.println(RecursionClasswork.groupSum(0, nums, 16));
 System.out.println(RecursionClasswork.groupSum(0, nums, 15));
 System.out.println(RecursionClasswork.groupSum(0, nums, 0));
 System.out.println(RecursionClasswork.groupSum(0, nums, 3));
+System.out.println(RecursionClasswork.groupSum(0, empty, 15));
+System.out.println(RecursionClasswork.groupSum(0, empty, 0));
 }
 }
