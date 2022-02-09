cb2916f1b0b0723b4660993d98426fe63198055c
HarryZ <hzhu30@stuy.edu>
2022-02-09 10:11:11 -0500

    I made a special case for empty arrays.

--- Classwork/RecursionClasswork.java
+++ Classwork/RecursionClasswork.java
@@ -5,10 +5,14 @@ public static void main(String[] args) {
 }
 
 public static boolean groupSum(int start, int[] nums, int target) {
+  if (nums.length == 0) {
+    return target == 0;
+  }
+
   if (target != 0) {
-if (start == nums.length - 1) {
-  return false;
-}
+    if (start == nums.length - 1) {
+      return false;
+    }
     return (groupSum(start + 1, nums, target) || groupSum(start + 1, nums, target - nums[start]));
   } else {
     return true;
