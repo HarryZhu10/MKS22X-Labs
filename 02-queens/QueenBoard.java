9b0223f8c3d139bc4131bb7314d4901432fb1470
HarryZ <hzhu30@stuy.edu>
2022-02-14 11:40:32 -0500

    I created an if statement that would determine if a board is available. If it is, then I would change it to a -1. I created a nested for loop that was supposed to add one to all the boards below and right diagonal of the queen. It isn't working right now since the loop is adding multiple times. I am planning on using a while loop.

--- 02-queens/QueenBoard.java
+++ 02-queens/QueenBoard.java
@@ -24,8 +24,11 @@ public class QueenBoard {
                  if (board[i][x] == -1) {
                    result += 'Q';
                  } else {
-                   //use this version if you are working on your lab (result += board[i][x];)
-                   result += '_';
+                   //use this version if you are working on your lab
+                   result += board[i][x];
+
+
+                  // result += '_';
                  }
 
                  if (x != board[i].length - 1) {
@@ -40,42 +43,52 @@ public class QueenBoard {
              return result;
            }
            /**
-           *@return true when the queen added correctly, false Otherwise
-           *@postcondition the board is only changed when the function returns true
-           * in which case the queen is added and all it's threatened positions are incremented
+             *@return true when the queen added correctly, false Otherwise
+             *@precondition r and c are valid indices of the board array
+             *@postcondition the board is only changed when the function returns true
+             * in which case the queen is added and all it's threatened positions are incremented
+             */
+             public boolean addQueen(int r, int c){
+               if (board[r][c] == 0) {
+                 board[r][c] = -1;
+                 for (int i = r; i < board.length; i ++) {
+                   board[r + 1][c] += 1;
+                   for (int x = c; x < board[i].length; x ++) {
+                     board[r + 1][c + 1] += 1;
+                   }
+                 }
+                 return true;
+               } else {
+                 return false;
+               }
+             }
 
-           private boolean addQueen(int r, int c){
+             /**Remove the queen that was added to r,c
+             *@precondition r and c are valid indices of the board array and there is a queen at position r,c
+             *@postcondition the board is modified to remove that queen and all it's
+             *threatened positions are decremented
 
-           }
+             private void removeQueen(int r, int c){
 
-           /**Remove the queen that was added to r,c
-           *@precondition there is a queen at position r,c
-           *@postcondition the board is modified to remove that queen and all it's
-           *threatened positions are decremented
+             }
 
-           private void removeQueen(int r, int c){
+             /**Find the first solution configuration possible for this size board. Start by placing
+             *  the 1st queen in the top left corner, and each new queen in the next ROW. When backtracking
+             *  move the previous queen to the next valid space. This means everyone will generate the same
+             *  first solution.
+             *@postcondition: the board remains in a solved state.
+             *@return false when the board is not solveable and leaves the board filled with zeros;
+             *        returns true when the board is solveable, and leaves the board in a solved state
+             *@throws IllegalStateException when the board starts with any non-zero value (e.g. you solved a 2nd time.)
 
-           }
+             public boolean solve(){}
 
-           /**Find the first solution configuration possible for this size board. Start by placing
-           *  the 1st queen in the top left corner, and each new queen in the next ROW. When backtracking
-           *  move the previous queen to the next valid space. This means everyone will generate the same
-           *  first solution.
-           *@postcondition: the board remains in a solved state.
-           *@return false when the board is not solveable and leaves the board filled with zeros;
-           *        returns true when the board is solveable, and leaves the board in a solved state
-           *@throws IllegalStateException when the board starts with any non-zero value (e.g. you solved a 2nd time.)
+             /**Find all possible solutions to this size board.
+             *@return the number of solutions found, and leaves the board filled with only 0's
+             *@throws IllegalStateException when the board starts with any non-zero value (e.g. you ran solve() before this method)
 
-           public boolean solve(){}
-
-           /**Find all possible solutions to this size board.
-           *@return the number of solutions found, and leaves the board filled with only 0's
-           *@throws IllegalStateException when the board starts with any non-zero value (e.g. you ran solve() before this method)
-
-           public int countSolutions(){}
+             public int countSolutions(){}
 
 
-*/
-
-
+               */
 }
