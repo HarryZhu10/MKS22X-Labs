6d91566e7130165e934cc48435bc293ae5d9a92a
HarryZ <hzhu30@stuy.edu>
2022-02-16 09:51:45 -0500

    I included the newer testing so that you are able to do the size and delay in java.

--- 02-queens/Driver.java
+++ 02-queens/Driver.java
@@ -1,8 +1,33 @@
 public class Driver {
 public static void main(String[] args) {
   QueenBoard test = new QueenBoard(4,4);
-  
+
+  QueenBoard test2 = new QueenBoard(3,3);
+  /*
    System.out.println(test.toString());
+   test.addQueen(0,3);
+   System.out.println(test.toString());
+   test.removeQueen(0,3);
+   System.out.println(test.toString());
+*/
+
+int SIZE = 8;
+  if(args.length > 0){
+    SIZE = Integer.parseInt(args[0]);
+  }
+  QueenBoard b = new QueenBoard(SIZE);
+  if(args.length > 1){
+    b.setAnimate(true);
+    b.setDelay(Integer.parseInt(args[1]));
+  }
+  System.out.println(Text.CLEAR_SCREEN);
+  System.out.println(Text.HIDE_CURSOR);
+  System.out.println(Text.go(1,1));
+  b.solve();
+  System.out.println(Text.RESET);
+  System.out.println(Text.go(1,1));
+  System.out.println(b);
+
 }
 
 }
