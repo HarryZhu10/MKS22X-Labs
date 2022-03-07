6daf3a7d90c470406c8bbc37fbf229717f587cf9
HarryZ <hzhu30@stuy.edu>
2022-03-06 21:32:33 -0500

    I created a helper method that counted the amount of open neighbors.

--- 03-maze/MazeGenerator.java
+++ 03-maze/MazeGenerator.java
@@ -1,12 +1,40 @@
 public class MazeGenerator {
 //you still need to check if the space have fewer than 2 open neighbors
+
+public static int openNeighbors(char[][]maze, int row, int col) {
+  int sum = 0;
+  if (maze[row][col + 1] == ' ') {
+    sum ++;
+  }
+
+  if (maze[row][col - 1] == ' ') {
+    sum ++;
+  }
+
+  if (maze[row + 1][col] == ' ') {
+    sum ++;
+  }
+
+  if (maze[row - 1][col] == ' ') {
+    sum ++;
+  }
+  return sum;
+}
+
+
 public static boolean safeCarve (char[][]maze, int row, int col) {
+    if ()
   return row != 0 && row != maze.length - 1 && col != 0 && col != maze[row].length - 1;
 }
 
 
 public static void generate(char[][]maze, int startrow, int startcol) {
-for (int i = 0; i < maze.length; i ++) {
+  if (safeCarve(maze, startrow, startcol) == true) {
+    //You have to check the open neighbors
+  }
+
+
+//for (int i = 0; i < maze.length; i ++) {
   for (int x = 0; x < maze[i].length; x ++) {
     if (startrow == i && startcol == x) {
       maze[i][x] = 'S';
