40050af7b3d988812ca4c2730bd672dd833d2d16
HarryZ <hzhu30@stuy.edu>
2022-03-10 16:28:24 -0500

    I created a helper method stomp.It will use a for loop that checks the 3 by 3 grid in the specified spot for the maximum value. Then you subtract the values in that area to reach the target value.

--- 04-usaco/Bronze.java
+++ 04-usaco/Bronze.java
@@ -17,13 +17,40 @@ public static long solve(String filename) {
     instructions.add(in.nextLine());
   }
 
-
+while (instructions.size() > 0) {
+  String ins = instructions.remove(0);
+  stomp(ins.substring(0, 1), ins.substring(1, 2), ins.substring(2), pasture);
+}
 
 }
 
 
+public static void stomp (int row, int col, int stompAmount, char[][] pasture) {
+  int max = 0;
+  for (int i = row - 1; i < pasture.length && i < row + 2; i ++) {
+    for (int x = col - 1; x < pasture[i].length && x < col + 2; x ++) {
+      if (pasture[i][x] > max) {
+        max = pasture[i][x];
+      }
+    }
+  }
 
 
+int targetAmount = max - stompAmount;
+
+
+for (int i = row - 1; i < pasture.length && i < row + 2; i ++) {
+  for (int x = col - 1; x < pasture[i].length && x < col + 2; x ++) {
+    if (pasture[i][x] > targetAmount) {
+      pasture[i][x] = targetAmount;
+    }
+  }
+}
+
+}
+
+
+}
 
 
 
