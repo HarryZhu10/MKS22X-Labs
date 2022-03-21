cfafd1ee0b06224811e58e3b29e4bfef9fc3c564
HarryZ <hzhu30@stuy.edu>
2022-03-21 13:58:33 -0400

    I fixed and added a lot of changes so that the last number wouldn't be excluded.

--- 06-merge/Merge.java
+++ 06-merge/Merge.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Merge {
 
 
@@ -10,28 +12,89 @@ public static int[] merge(int[] left, int[] right) {
   int[] merged = new int[left.length + right.length];
   int rIndex = 0;
   int lIndex = 0;
-
-  for (int i = 0; i < merged.length; i ++) {
+  int count = 0;
+  for (int i = 0; count < merged.length && rIndex < right.length && lIndex < left.length; count ++) {
     if (left[lIndex] < right[rIndex]) {
-      merged[i] = left[lIndex];
+      merged[count] = left[lIndex];
+      lIndex ++;
+      System.out.println(Arrays.toString(merged));
+    } else if (left[lIndex] > right[rIndex]) {
+      merged[count] = right[rIndex];
+      rIndex ++;
+            System.out.println(Arrays.toString(merged));
+    } else {
+      merged[count] = right[rIndex];
+      merged[count + 1] = left[lIndex];
+      rIndex ++;
+      lIndex ++;
+      count ++;
+            System.out.println(Arrays.toString(merged));
+    }
+  }
+
+if (rIndex == right.length) {
+  for (int i = lIndex; i < left.length; i ++) {
+    merged[count] = left[lIndex];
+    lIndex ++;
+    count ++;
+          System.out.println(Arrays.toString(merged));
+  }
+  } else if (lIndex == left.length) {
+    for (int i = rIndex; i < right.length; i ++) {
+    merged[count] = right[rIndex];
+    rIndex ++;
+    count ++;
+          System.out.println(Arrays.toString(merged));
+  }
+}
+  return merged;
+}
+
+/*
+
+for (int i = 0; i < merged.length; i ++) {
+if (right.length > left.length) {
+  while(rIndex < right.length && lIndex < left.length) {
+
+}
+} else if (right.length < left.length) {
+
+} else {
+
+}
+}
+
+*/
+
+
+public static void merge(int[] destination, int [] left, int[] right) {
+  int rIndex = 0;
+  int lIndex = 0;
+
+  for (int i = 0; i < destination.length; i ++) {
+    if (left[lIndex] < right[rIndex]) {
+      destination[i] = left[lIndex];
       lIndex ++;
     } else if (left[lIndex] > right[rIndex]) {
-      merged[i] = right[rIndex];
+      destination[i] = right[rIndex];
       rIndex ++;
     } else {
-      merged[i] = right[rIndex];
-      merged[i + 1] = left[lIndex];
+      destination[i] = right[rIndex];
+      destination[i + 1] = left[lIndex];
       rIndex ++;
       lIndex ++;
       i ++;
     }
   }
-  return merged;
 }
 
 
-public static void merge(int[] destination, int [] left, int[] right) {
+public static void main(String[] args) {
+  int[] test1 = {2,3,3,3,4,7};
+  int[] test2 = {3,5,5,6,8};
 
+System.out.println(Arrays.toString(merge(test2, test1)));
 }
+
 
 }
